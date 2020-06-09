5d35102467242b3e0ccccfd94ff8399742be874c
chaudharygarvit9 <garvit.agdav@gmail.com>
2020-06-09 22:32:14 +0530

    Add files via upload

---
+++ dataTypes.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x=12;
+	char a='a';
+	double y=6.262e-10;
+	float z=4.32;
+	bool isdigit=true;
+	cout<<x<<" "<< a<<" "<<y<<" "<<z<<" "<<isdigit;
+}
